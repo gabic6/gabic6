2301c0f7c9e59ba9b90987b83774c73cf47267ba
gabiiwa <gabriele.cesar.iwashima@ice.ufjf.br>
2021-11-29 00:01:20 -0300

    retirando arquivos desnecessários

--- exericio_rmi/AdicaoInterface.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-/**
- * Interface da operação de adição
- */
-public interface AdicaoInterface extends Remote {
-    public long add(long a, long b) throws RemoteException;
-}
--- exericio_rmi/SubtracaoInterface.java
+++
@@ -1,7 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface SubtracaoInterface extends Remote
-{
-    public long subtracao(long a, long b) throws RemoteException;
-}
